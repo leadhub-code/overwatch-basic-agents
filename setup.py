1725f48dc64acab9a18cac6bcecb0cfdb7c8f38f
Petr Messner <petr.messner@gmail.com>
2018-02-26 13:10:08 +0100

    version 0.0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='overwatch-basic-agents',
-    version='0.0.1',
+    version='0.0.2',
     author='Petr Messner',
     author_email='petr.messner@gmail.com',
     license='MIT',
